6a118c845a3dea9cc67aa4f342f04f9eb08acf3f
Li Weinan <li.weinan@rea-group.com>
2019-06-17 17:37:26 +0800

    repalce list with map

--- src/main/java/args/Schema.java
+++ src/main/java/args/Schema.java
@@ -2,16 +2,20 @@ package args;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Schema {
     private List<Flag> flags = new ArrayList<>();
+    private Map<String, Flag> flagMap = new HashMap<>();
 
     public void addFlag(String flag, String type) {
         this.flags.add(new Flag(flag, type));
+        this.flagMap.put(flag, new Flag(flag, type));
     }
 
     public Flag getFlag(String flag) {
-        return flags.stream().filter(f -> f.getName().equals(flag)).findFirst().get();
+        return flagMap.get(flag);
     }
 }
