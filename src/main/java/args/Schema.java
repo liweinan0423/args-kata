e79cdcab8a04b092567a3c4b3585ab29c9c466b7
Li Weinan <li.weinan@rea-group.com>
2019-06-17 17:37:50 +0800

    remove list property

--- src/main/java/args/Schema.java
+++ src/main/java/args/Schema.java
@@ -7,11 +7,9 @@ import java.util.List;
 import java.util.Map;
 
 public class Schema {
-    private List<Flag> flags = new ArrayList<>();
     private Map<String, Flag> flagMap = new HashMap<>();
 
     public void addFlag(String flag, String type) {
-        this.flags.add(new Flag(flag, type));
         this.flagMap.put(flag, new Flag(flag, type));
     }
 
