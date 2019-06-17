920908d2b94279ef89c9386ba13c9425c45f34bb
Li Weinan <li.weinan@rea-group.com>
2019-06-17 17:45:31 +0800

    rename test case

--- src/test/java/SchemaTest.java
+++ src/test/java/SchemaTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class ArgsTest {
+public class SchemaTest {
     @Test
     public void boolean_flag() {
         Schema schema = new Schema();
